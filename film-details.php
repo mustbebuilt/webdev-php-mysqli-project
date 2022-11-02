b6319b85ebfa5c53f433d711125c2f6564107c8b
mustbebuilt <martin@mustbebuilt.co.uk>
2022-11-02 11:19:55 +0000

    Tidy up

--- film-details.php
+++ film-details.php
@@ -48,7 +48,7 @@
       </div>
 <div class="sideBar">
         <h3>Featured Film</h3>
-        <div> <img src="images/babadook.jpg" alt="Babadook"> </div>
+        <div> <img src="images/babadook.jpg" alt="Babadook" /> </div>
         <p>Info Here</p>
       </div>
     </section>
--- thankyou.php
+++ thankyou.php
@@ -46,7 +46,7 @@
 
 <div class="sideBar">
         <h3>Featured Film</h3>
-        <div> <img src="images/babadook.jpg" alt="Babadook"> </div>
+        <div> <img src="images/babadook.jpg" alt="Babadook" /> </div>
         <p>Info Here</p>
       </div>
     </section>
