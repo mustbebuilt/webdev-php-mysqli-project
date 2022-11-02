e42c51093d37513cf00bba4aa9194150c078dd50
mustbebuilt <martin@mustbebuilt.co.uk>
2022-11-02 11:05:03 +0000

    Update catalogue.php

--- catalogue.php
+++ catalogue.php
@@ -46,6 +46,11 @@
                 <div>
                 <!-- Film Listing Here -->
                 </div>
+		<div class="sideBar">
+                <h3>Featured Film</h3>
+                <div> <img src="images/babadook.jpg" alt="Babadook"> </div>
+                <p>Info Here</p>
+                </div>
 
               </section>
         </main>
