1e635aba76c1613dc32080b1a1bbd8970c04ae05
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2022-11-03 11:48:58 +0000

    Set up 2022

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,6 @@
               <li><a href="index.php">Home</a></li>
               <li><a href="catalogue.php">Catalogue</a></li>
               <li><a href="search.php">Search</a></li>
-              <li><a href="contact.php">Contact Us</a></li>
             </menu>
           </nav>
         </div>
